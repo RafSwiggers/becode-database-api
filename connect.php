e7a2c8073da552320b21201aebb5befa7a11642e
RafSwiggers <rafswiggers@gmail.com>
2019-08-09 11:46:38 +0200

    connect and display the current DB - Table

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "NoteBase";
+$title = $_GET['title'];
+$content = $_GET['content'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT id, title, content FROM notes";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Title: " . $row["title"]. " Note : " . $row["content"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
