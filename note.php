2aebbb65a464000ccea9ccd4c105c2295016de6f
RafSwiggers <rafswiggers@gmail.com>
2019-09-16 09:39:01 +0200

    finish app

--- note.php
+++ note.php
@@ -11,82 +11,75 @@ $update = filter_var($_POST['update'],FILTER_SANITIZE_STRING);
 $conn = new mysqli($servername, $username, $password, $dbname);
 $result = [];
 // Check connection
+//add notes
+if (isset($_GET['title'])){
+    if(isset($_POST['content'])){
+        $sql = "INSERT INTO notes (title, content)
+        VALUES ('$title', '$content')";
+    
+        if ($conn->query($sql)) {
+        echo " New record created successfully ";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+  
+}
+}
+// give list 
+if(isset($_GET['list'])){
+    $sql = "SELECT title FROM notes"; 
+    $result = $conn->query($sql);
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $jsonresult = json_encode($row);
+            print_r($jsonresult);
+        } 
+    } else {
+        echo "no results";
+    }
+    }
+// delete notes 
+$delete = $_GET['delete']; 
+if(isset($_GET['delete'])){
+    $sql = "DELETE FROM notes WHERE title='$delete'";
+    if ($conn->query($sql)) {
+        echo " deleted ";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    }
+//update 
+$update = $_GET['update']; 
+$newtext = $_GET['newtext'];
+if(isset($_GET['update'])){
+    $sql = "UPDATE notes SET content='$newtext' WHERE title='$update'";
+    if ($conn->query($sql)) {
+        echo " updated ";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+// get 
+$select = $_GET['select']; 
+if(isset($_GET['select'])){
+    $sql = "SELECT *  FROM notes WHERE  title='$select'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $jsonresult = json_encode($row);
+        print_r($jsonresult);
+    } 
+} else {
+    echo "no results";
+}
+}
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-if(isset($_GET['title'])){
-if(isset($_POST['content'])){
-        $sql = "INSERT INTO notes (title, content)
-        VALUES ('$title', '$content')";
-
-        if ($conn->query($sql) === TRUE) {
-        return "New record created successfully";
-
-        } else {
-        return "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-    } elseif (isset($_POST['update'])) {
-        $sql="UPDATE notes
-        SET content = '$update'
-        WHERE Title = '$title'";
-        if ($conn->query($sql) === TRUE) {
-            return "Succesfully updated ".$title;
-            } else {
-            return "Error: " . $sql . "<br>" . $conn->error;
-            }
-}
-}
-
-if (isset($_GET['show'])){
-    $sql = "SELECT * FROM notes";
-    $result = $conn->query($sql);
-    $i = 0;
-    $array_result = [];
-    
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-                $array_result[$row["Title"]] = $row['content'];
-                $i++;
-      
-        }
-        return json_encode($array_result); 
-    } else {
-        return "0 results";
-    }
-}
-
-if (isset($_GET['search'])){
-    $sql = "SELECT * FROM notes WHERE Title ='$search'";
-    $result = $conn->query($sql);
-    $i = 0;
-    $array_result = [];
-    
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-                $array_result[$row["Title"]] = $row['content'];
-                $i++;
-      
-        }
-        return json_encode($array_result); 
-    } else {
-        return "0 results";
-    }
-};
-if(isset($_GET['delete'])){
-    $sql = "DELETE FROM notes WHERE title = '$delete'";
-    if ($conn->query($sql) === TRUE) {
-    return "Record deleted";
-
-    } else {
-    return "Error: " . $sql . "<br>" . $conn->error;
-    }
-    
-}
-
-
-$conn->close();
+echo " Connected successfully ";
+//do
+mysqli_close($conn);
 ?>
